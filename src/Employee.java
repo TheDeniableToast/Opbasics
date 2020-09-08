c8a781eac49a014d0594d33c97ba8d435f3c1d19
filip.norberg <filipnorberg02@outlook.com>
2020-09-08 13:39:52 +0200

    added more classes

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary){
+
+    }
+}
