8995fc0d4b7714fcb877b2da0900be8ad8d4e7cd
filip.norberg <filipnorberg02@outlook.com>
2020-09-14 11:04:00 +0200

    worked on employee class

--- src/Employee.java
+++ src/Employee.java
@@ -5,7 +5,32 @@ public class Employee {
     private String lastName;
     private int salary;
 
-    public Employee(int id, String firstName, String lastName, int salary){
-
+    public Employee(){
+        int id;
+        String firstName;
+        String lastName;
+        int salary;
     }
+
+    public int getId;
+    public String getFirstName;
+    public String getLastName;
+    public String getName() {
+        return firstName + lastName;
+    }
+
+    public int getSalary;
+    public void setSalary() {
+        int salary;
+    }
+    public int getAnnualSalary() {
+        return salary*12;
+    }
+
+    public int raiseSalary(int percent) {
+        return salary + percent;
+    }
+
+
+
 }
